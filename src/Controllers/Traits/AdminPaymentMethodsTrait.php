17ee8cc2b53f05ebb0fab02d33324a7516600a7d
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-06-29 15:03:57 +0300

    add controller traits

---
+++ src/Controllers/Traits/AdminPaymentMethodsTrait.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ByTIC\Payments\Controllers\Traits;
+
+use ByTIC\Common\Records\Record;
+use ByTIC\Common\Records\Records;
+use ByTIC\Payments\Models\Methods\Traits\RecordsTrait;
+use ByTIC\Payments\Models\Methods\Traits\RecordTrait;
+use Nip\Request;
+
+/**
+ * Class AdminPaymentMethodsTrait
+ * @package ByTIC\Common\Payments\Controllers\Traits
+ */
+trait AdminPaymentMethodsTrait
+{
+
+    public function deleteFile()
+    {
+        $item = $this->getModelFromRequest();
+        $fileName = $this->getRequest()->get('file');
+
+        $type = 'error';
+        $message = $this->getModelManager()->getMessage('deleteFile.error');
+        if ($fileName) {
+            $files = $item->findFiles();
+            if ($files[$fileName]) {
+                $files[$fileName]->delete();
+                $type = 'success';
+                $message = $this->getModelManager()->getMessage('deleteFile.success');
+            } else {
+                $message = $this->getModelManager()->getMessage('deleteFile.no-file');
+            }
+        } else {
+            $message = $this->getModelManager()->getMessage('deleteFile.no-filename');
+        }
+        $this->flashRedirect($message, $item->getViewURL(), $type);
+    }
+
+    /**
+     * @param bool $key
+     * @return Record|RecordTrait
+     */
+    abstract protected function getModelFromRequest($key = false);
+
+    /**
+     * @return Request
+     */
+    abstract protected function getRequest();
+
+    /**
+     * @return Records|RecordsTrait
+     */
+    abstract protected function getModelManager();
+
+    /**
+     * @param $message
+     * @param $url
+     * @param string $type
+     * @param bool $name
+     * @return mixed
+     */
+    abstract protected function flashRedirect($message, $url, $type = 'success', $name = false);
+
+    public function delete()
+    {
+        $item = $this->getModelFromRequest();
+
+        $deleteMessage = $item->canDelete();
+        if ($deleteMessage !== true) {
+            $url = $item->getURL();
+            $this->_flashName = $this->getModelManager()->getController();
+            $this->flashRedirect($deleteMessage, $url, 'error', $this->_flashName);
+        }
+
+        parent::delete();
+    }
+}
---
+++ src/Controllers/Traits/PurchaseControllerTrait.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace ByTIC\Payments\Controllers\Traits;
+
+use ByTIC\Common\Payments\Gateways\Providers\AbstractGateway\Message\CompletePurchaseResponse;
+use ByTIC\Common\Payments\Gateways\Providers\AbstractGateway\Message\ServerCompletePurchaseResponse;
+use ByTIC\Common\Records\Record;
+use ByTIC\Payments\Gateways\Manager as GatewaysManager;
+use ByTIC\Payments\Models\Purchase\Traits\IsPurchasableModelTrait;
+use Nip\Records\RecordManager;
+use Nip\Request;
+
+/**
+ * Class PurchaseControllerTrait
+ * @package ByTIC\Common\Payments\Controllers\Traits
+ *
+ * @method IsPurchasableModelTrait checkItem
+ */
+trait PurchaseControllerTrait
+{
+
+    public function redirectToPayment()
+    {
+        $model = $this->getModelFromRequest();
+        $request = $model->getPurchaseRequest();
+        $response = $request->send();
+        $response->getView()->set('subtitle', $model->getPurchaseName());
+        $response->getView()->set('item', $model);
+        $response->getView()->set('response', $model);
+        echo $response->getRedirectResponse()->getContent();
+        die();
+    }
+
+    /**
+     * @param bool|array $key
+     * @return Record|IsPurchasableModelTrait
+     */
+    abstract protected function getModelFromRequest($key = false);
+
+    public function confirm()
+    {
+        $response = $this->getConfirmActionResponse();
+        $model = $response->getModel();
+        if ($model) {
+            $response->processModel();
+        }
+        $this->confirmProcessResponse($response);
+        $response->send();
+        die();
+    }
+
+    /**
+     * @return CompletePurchaseResponse
+     */
+    protected function getConfirmActionResponse()
+    {
+        /** @var CompletePurchaseResponse $response */
+        $response = GatewaysManager::instance()->detectItemFromHttpRequest(
+            $this->getModelManager(),
+            'completePurchase',
+            $this->getRequest()
+        );
+
+        if (($response instanceof CompletePurchaseResponse) === false) {
+            $this->dispatchAccessDeniedResponse();
+        }
+        return $response;
+    }
+
+    /**
+     * @return RecordManager
+     */
+    protected abstract function getModelManager();
+
+    /**
+     * @return Request
+     */
+    abstract protected function getRequest();
+
+    abstract protected function dispatchAccessDeniedResponse();
+
+    /**
+     * @param CompletePurchaseResponse $response
+     * @return void
+     */
+    abstract protected function confirmProcessResponse($response);
+
+    public function ipn()
+    {
+        $response = $this->getIpnActionResponse();
+        $model = $response->getModel();
+        if ($model) {
+            $response->processModel();
+        }
+        $this->ipnProcessResponse($response);
+        $response->send();
+        die();
+    }
+
+    /**
+     * @return ServerCompletePurchaseResponse
+     */
+    protected function getIpnActionResponse()
+    {
+        /** @var ServerCompletePurchaseResponse $response */
+        $response = GatewaysManager::instance()->detectItemFromHttpRequest(
+            $this->getModelManager(),
+            'serverCompletePurchase',
+            $this->getRequest()
+        );
+
+        if (($response instanceof ServerCompletePurchaseResponse) === false) {
+            $this->dispatchAccessDeniedResponse();
+        }
+        return $response;
+    }
+
+    /**
+     * @param ServerCompletePurchaseResponse $response
+     * @return void
+     */
+    abstract protected function ipnProcessResponse($response);
+
+    /**
+     * @return GatewaysManager
+     */
+    protected function getGatewaysManager()
+    {
+        return GatewaysManager::instance();
+    }
+}
