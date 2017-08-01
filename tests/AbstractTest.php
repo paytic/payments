da52c860ce209f4cc132067fafae2810059982f6
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-08-01 10:52:58 +0300

    initial add tests

---
+++ tests/AbstractTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ByTIC\Payments\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTest
+ */
+abstract class AbstractTest extends TestCase
+{
+    protected $object;
+}
---
+++ tests/Gateways/GatewayFactoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ByTIC\Payments\Tests\Gateways;
+
+use ByTIC\Payments\Gateways\GatewayFactory;
+use ByTIC\Payments\Tests\AbstractTest;
+
+/**
+ * Class GatewayFactoryTest
+ * @package ByTIC\Payments\Tests\Gateways
+ */
+class GatewayFactoryTest extends AbstractTest
+{
+    /**
+     * @dataProvider getNameProvider
+     * @param $short
+     * @param $class
+     */
+    public function testGetGatewayClassName($short, $class)
+    {
+        self::assertEquals(
+            $class,
+            GatewayFactory::getGatewayClassName($short)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getNameProvider()
+    {
+        return [
+            ['Payu', '\ByTIC\Payments\Gateways\Providers\Payu\Gateway'],
+            ['Mobilpay', '\ByTIC\Payments\Gateways\Providers\Mobilpay\Gateway'],
+            ['Euplatesc', '\ByTIC\Payments\Gateways\Providers\Euplatesc\Gateway'],
+            ['Twispay', '\ByTIC\Payments\Gateways\Providers\Twispay\Gateway']
+        ];
+    }
+}
