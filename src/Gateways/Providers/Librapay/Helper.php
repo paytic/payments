931a045ab289291133d00911e5759b1e2c2cbf95
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-08-09 08:43:03 +0300

    update helper for librapay to decorate order id

---
+++ src/Gateways/Providers/Librapay/Helper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ByTIC\Payments\Gateways\Providers\Librapay;
+
+/**
+ * Class Helper
+ * @package ByTIC\Payments\Gateways\Providers\Librapay
+ */
+class Helper
+{
+    /**
+     * Required because librapay API needs order id at least 6 digits no leading zero
+     * @param $idOrder
+     * @return mixed
+     */
+    public static function encodeOrderId($idOrder)
+    {
+        if ($idOrder < 100000) {
+            $idOrder = '99999999999999'.str_pad($idOrder, 5, "0", STR_PAD_LEFT);
+        }
+
+        return $idOrder;
+    }
+
+    /**
+     * @param $idOrder
+     * @return int
+     */
+    public static function decodeOrderId($idOrder)
+    {
+        $idString = (string) $idOrder;
+        if (strlen($idString) == 19) {
+            var_dump($idString);
+            $idOrder = str_replace('99999999999999', '', $idString);
+        }
+        return $idOrder;
+    }
+}
