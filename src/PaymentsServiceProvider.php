ab88d4237f3dcf0a4dc7e8b5b857c18972bddbfa
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-08-09 09:28:02 +0300

    fix tests

--- src/PaymentsServiceProvider.php
+++ src/PaymentsServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ByTIC\Payments;
 
+use Nip\Container\ServiceProviders\Providers\AbstractSignatureServiceProvider;
+
 /**
  * Class PaymentsServiceProvider
  * @package ByTIC\Payments
@@ -9,4 +11,26 @@ namespace ByTIC\Payments;
 class PaymentsServiceProvider extends AbstractSignatureServiceProvider
 {
 
+    /**
+     * Returns a boolean if checking whether this provider provides a specific
+     * service or returns an array of provided services if no argument passed.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        // TODO: Implement provides() method.
+    }
+
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
 }
