30043275eec8ac02c2a2d68f14505aa05734af07
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-04-30 19:06:50 +0300

    add more tests

---
+++ tests/Gateways/Providers/AbstractGateway/Message/CompletePurchaseResponseTestTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ByTIC\Payments\Tests\Gateways\Providers\AbstractGateway\Message;
+
+trait CompletePurchaseResponseTestTrait
+{
+    public function testHasConfirmHtmlTraitTrait()
+    {
+        $response = $this->getNewResponse();
+
+        static::assertTrue(method_exists($response, 'getViewFile'));
+        static::assertTrue(method_exists($response, 'getView'));
+    }
+
+    abstract protected function getNewResponse();
+}
---
+++ tests/Gateways/Providers/Payu/Message/CompletePurchaseResponseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ByTIC\Payments\Tests\Gateways\Providers\Payu\Message;
+
+use ByTIC\Payments\Gateways\Providers\Payu\Message\CompletePurchaseResponse;
+use ByTIC\Payments\Gateways\Providers\Payu\Message\CompletePurchaseRequest;
+use ByTIC\Payments\Tests\AbstractTest;
+use ByTIC\Payments\Tests\Gateways\Providers\AbstractGateway\Message\CompletePurchaseResponseTestTrait;
+use Symfony\Component\HttpFoundation\Request;
+
+class CompletePurchaseResponseTest extends AbstractTest
+{
+    use CompletePurchaseResponseTestTrait;
+
+    protected function getNewResponse()
+    {
+
+        $request = new CompletePurchaseRequest($this->client, new Request());
+
+        return new CompletePurchaseResponse($request, []);
+    }
+}
