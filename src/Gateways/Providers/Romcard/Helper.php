12dccf7216608cb4d9a86d6dc9c332aef28f3e7a
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-04-30 14:22:16 +0300

    fix helper romcard extends

--- src/Gateways/Providers/Romcard/Helper.php
+++ src/Gateways/Providers/Romcard/Helper.php
@@ -6,6 +6,6 @@ namespace ByTIC\Payments\Gateways\Providers\Romcard;
  * Class Helper
  * @package ByTIC\Payments\Gateways\Providers\Romcard
  */
-class Helper extends ByTIC\Omnipay\Romcard\Helper
+class Helper extends \ByTIC\Omnipay\Romcard\Helper
 {
 }
