82f0fc0a5c8f62d15c3f72963cb801b5d2390c69
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-09-12 21:15:51 +0300

    feat: update repos to new symfony 6 components

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,7 @@ $configData = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECT
 
 require dirname(__DIR__) . '/vendor/paytic/payments-tests/src/boostrap/bootstrap.php';
 
+translator()->setLocale('ro');
+
 putenv('PLATIONLINE_PUBLIC_KEY=' . gzinflate(base64_decode(envVar('PLATIONLINE_PUBLIC_KEY'))));
 putenv('PLATIONLINE_PRIVATE_KEY=' . gzinflate(base64_decode(envVar('PLATIONLINE_PRIVATE_KEY'))));
