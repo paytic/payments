d2c63fdfa929237fa018ae81aafffb24e7fb8bfe
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-10-24 23:49:05 +0300

    feat: initial add payment method links

--- src/Models/Purchases/PurchasesTrait.php
+++ src/Models/Purchases/PurchasesTrait.php
@@ -3,8 +3,10 @@
 namespace Paytic\Payments\Models\Purchases;
 
 use ByTIC\Models\SmartProperties\RecordsTraits\HasStatus\RecordsTrait;
+use Nip\Config\Config;
 use Paytic\Payments\Models\Purchase\Traits\IsPurchasableRepositoryTrait;
 use Paytic\Payments\Models\Transactions\Statuses\Pending;
+use Paytic\Payments\Utility\PaymentsModels;
 
 /**
  * Trait PurchasesTrait
@@ -34,5 +36,14 @@ trait PurchasesTrait
     public function getDefaultStatus(): string
     {
         return Pending::NAME;
+    }
+
+    /**
+     * @return mixed|Config
+     * @throws Exception
+     */
+    protected function generateTable()
+    {
+        return config('payments.tables.' . PaymentsModels::PURCHASES, Purchases::TABLE);
     }
 }
